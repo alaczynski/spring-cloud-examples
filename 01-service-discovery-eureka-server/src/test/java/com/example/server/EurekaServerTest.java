4c597cb34ed4bc8a49b875ebfc7209e7be84a732
alaczynski <adam.laczynski@gmail.com>
2016-12-17 23:47:24 +0100

    upgrade spring boot to 1.4.2 and spring cloud to Camden.SR3

--- 01-service-discovery-eureka-server/src/test/java/com/example/server/EurekaServerTest.java
+++ 01-service-discovery-eureka-server/src/test/java/com/example/server/EurekaServerTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,9 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
