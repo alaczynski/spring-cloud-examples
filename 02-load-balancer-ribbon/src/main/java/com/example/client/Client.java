f496ed19379d3d7c9d22a502fb28467b4169b7cc
alaczynski <adam.laczynski@gmail.com>
2016-12-18 16:14:18 +0100

    load balanced rest template example

--- 02-load-balancer-ribbon/src/main/java/com/example/client/Client.java
+++ 02-load-balancer-ribbon/src/main/java/com/example/client/Client.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import static org.springframework.web.util.UriComponentsBuilder.fromUri;
@@ -20,6 +22,8 @@ public class Client implements CommandLineRunner {
 
     @Autowired
     private LoadBalancerClient client;
+    @Autowired
+    private RestTemplate restTemplate;
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Client.class);
@@ -29,11 +33,23 @@ public class Client implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
+        logger.info("LoadBalancerClient usage...");
         for (int i = 0; i < 100; i++) {
             ServiceInstance serviceInstance = client.choose("server");
             String uri = fromUri(serviceInstance.getUri()).path("name").toUriString();
             String name = new RestTemplate().getForObject(uri, String.class);
             logger.info(name);
         }
+        logger.info("Load balanced RestTemplate usage...");
+        for (int i = 0; i < 100; i++) {
+            String name = restTemplate.getForObject("http://server/name", String.class);
+            logger.info(name);
+        }
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
