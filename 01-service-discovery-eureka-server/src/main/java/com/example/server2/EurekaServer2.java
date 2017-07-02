213cd0033d345b24a4fc33ec1ff179f51ab1befa
alaczynski <adam.laczynski@gmail.com>
2017-07-02 21:34:52 +0200

    update mvnw

--- 01-service-discovery-eureka-server/src/main/java/com/example/server2/EurekaServer2.java
+++ 01-service-discovery-eureka-server/src/main/java/com/example/server2/EurekaServer2.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.server2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,11 +6,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServer1 {
+public class EurekaServer2 {
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(EurekaServer1.class);
-        application.setAdditionalProfiles("eureka-server-1");
+        SpringApplication application = new SpringApplication(EurekaServer2.class);
+        application.setAdditionalProfiles("eureka-server-2");
         application.run(args);
     }
 }
--- 01-service-discovery-eureka-server/src/test/java/com/example/server/EurekaServerTest.java
+++ 01-service-discovery-eureka-server/src/test/java/com/example/server/EurekaServerTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
